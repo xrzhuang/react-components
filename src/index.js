78d2dd82553a04e08ebe97a04c7711c58517ed53
prosper007 <onungwap@gmail.com>
2019-04-12 17:38:18 -0400

    deployed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+let num = 0;
+const main = $('#main');
+setInterval(() => {
+  main.html(`You've been on this page for ${num += 1} seconds`);
+}, 1000);
