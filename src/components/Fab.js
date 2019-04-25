ae295d66c5dbb5212717ad002b0c64f02697779a
overlake333 <taylor.m.olson.21@dartmouth.edu>
2019-04-25 01:48:04 -0400

    making dummy classes

--- src/components/Fab.js
+++ src/components/Fab.js
@@ -1,11 +1,5 @@
+
 import React from 'react';
-import Fab from '@material-ui/core/Fab';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
 import { withStyles } from '@material-ui/core/styles';
 
 const style = {
@@ -24,51 +18,11 @@ class AlertDialog extends React.Component {
     open: false,
   };
 
+  render() {
+    return (
 
-    handleClickOpen = () => {
-      this.setState({ open: true });
-    };
-
-    handleClose = (value) => {
-      this.setState({ open: false });
-    };
-
-    render() {
-      return (
-        <div>
-          <Fab
-            onClick={this.handleClickOpen}
-            id="button"
-            color="primary"
-            position="right-bottom"
-            style={style}
-          >
-          Call Tim
-          </Fab>
-          <Dialog
-            open={this.state.open}
-            onClose={this.handleClose}
-            aria-labelledby="alert-dialog-title"
-            aria-describedby="alert-dialog-description"
-          >
-            <DialogTitle id="alert-dialog-title">Need Help?</DialogTitle>
-            <DialogContent>
-              <DialogContentText id="alert-dialog-description">
-              I would suggest calling him.
-              </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={this.handleClose} color="primary">
-              not helpful
-              </Button>
-              <Button onClick={this.handleClose} color="primary" autoFocus>
-              SOS
-              </Button>
-            </DialogActions>
-          </Dialog>
-        </div>
-      );
-    }
+    );     
+  }
 }
 
 export default withStyles(style)(AlertDialog);
