c73df4b7b1131ee6d07b454079739191495740bf
Catherine Zhao <catherine.zhao.20@dartmouth.edu>
2019-04-25 01:37:18 -0400

    stripped nav bar

--- src/components/nav_bar.js
+++ src/components/nav_bar.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import { withStyles } from '@material-ui/core/styles';
+
+const NavBar = () => {
+
+  return (
+    // add app bar + tool bar here
+  );
+};
 
 const styles = {
   menu: {
@@ -17,25 +18,3 @@ const styles = {
     width: '100%',
   },
 };
-
-const NavBar = (props) => {
-  const { classes } = props;
-  return (
-    <div className="navDiv">
-      <AppBar position="static" color="default" className={classes.nav}>
-        <Toolbar>
-          <Typography variant="h6" color="inherit" className={classes.header}>
-            Dartmouth CS52 — 19S
-          </Typography>
-          <div className={classes.menu}>
-            <Button color="inherit">About</Button>
-            <Button color="inherit">Schedule</Button>
-            <Button color="inherit">Assignments</Button>
-          </div>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
-};
-
-export default withStyles(styles)(NavBar);
